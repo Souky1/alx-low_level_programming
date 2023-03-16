4b979765dd1197d9e066df7e42f9751743b4247f
Souky1 <soukaina.ouha@gmail.com>
2023-03-16 14:51:49 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,18 +4,8 @@
  * 
  * Return:Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle ");
-return (0);
-#include<stdio.h>
-/**
- * main-Entry point
- * 
- * Return:Always 0 (Success)
- */
-int main (void
-	{
-puts("\"Programming is like building a multilingual puzzle ");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
