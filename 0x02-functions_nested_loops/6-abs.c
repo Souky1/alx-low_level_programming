975252698f4079ee97b23d996bafbc22d63d9e99
Souky1 <soukaina.ouha@gmail.com>
2023-03-21 15:40:10 -0700

    asolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _abs -  the absolute value
+ * @a: compare integer
+ * Return: always 0
+ */
+
+int _abs(int)
+{
+	if (a < 0)
+		a = (-1) * a;
+	else if (a >= 0)
+		a = a;
+	return (a);
+}
+
