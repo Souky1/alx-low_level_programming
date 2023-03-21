7a3f45e2c3ee211180bff8b5e7c8298a8585ca2e
Souky1 <soukaina.ouha@gmail.com>
2023-03-21 14:06:58 -0700

    print lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,14 @@
 
 /**
  * print_alphabet - function to print alphabet lower case
- * 
- * Return: always 0        
-*/
+ * Return: always 0
+ */
 
 void print_alphabet(void)
 {
 	char i;
 
-	for (i = 'a'; i <= 'z'; ++i)
-		_putchar(i);
+	for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
 	_putchar('\n');
 }
