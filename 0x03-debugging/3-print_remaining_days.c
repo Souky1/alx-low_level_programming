363ca795f90853a61d35b7799b895271d0150fd5
Souky1 <soukaina.ouha@gmail.com>
2023-03-22 21:35:10 -0700

    like comment subscribe

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month
+* @year: year
+* Return: void
+*/
+
+void print_remaining_days(int month, int day, int year)
+{
+    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+    {
+        if (month >= 2 && day >= 60)
+        {
+            day++;
+        }
+
+        printf("Day of the year: %d\n", day);
+        printf("Remaining days: %d\n", 366 - day);
+    }
+    else
+    {
+        if (month == 2 && day == 60)
+        {
+            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+        }
+        else
+        {
+            printf("Day of the year: %d\n", day);
+            printf("Remaining days: %d\n", 365 - day);
+        }
+    }
+}
+
+carrie@ubuntu:/debugging$ 
+carrie@ubuntu:/debugging$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 3-convert_day.c 3-print_remaining_days.c 3-main_a.c -o 3-main_a 
+carrie@ubuntu:/debugging$ ./3-main_a
+Date: 04/01/1997
+Day of the year: 91
+Remaining days: 274
