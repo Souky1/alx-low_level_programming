d07c2183d5567ca05c1a8e9fb86e893af283c9a2
Souky1 <soukaina.ouha@gmail.com>
2023-03-23 18:03:31 -0700

    10-print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ * @size: print size
+ * Return: Always 0
+ */
+void print_triangle(int size)
+{
+	int i = 0, j = size, c;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (c = 0; c < j; c++)
+		{
+			while (i < j)
+			{
+				if ((i + c + 1) >= j)
+					_putchar('#');
+				else
+					_putchar(' ');
+				i++;
+			}
+			i = 0;
+			_putchar('\n');
+		}
+	}
+}
