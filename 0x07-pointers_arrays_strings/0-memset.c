bea15527a0ebd1a13a3ca28a5ee0a73bc38838eb
Souky1 <soukaina.ouha@gmail.com>
2023-04-03 10:04:39 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,15 +6,14 @@
  * @n: number of bytes to change
  * Return: changed array with new values for n bytes
  */
-
-char* _memset(char*s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
-	for(n > 0; i++)
+
+	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
 	}
 	return (s);
 }
- 
