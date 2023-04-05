5c91f8aa402966b6d432195d23c5661b9fc27319
Souky1 <soukaina.ouha@gmail.com>
2023-04-05 15:08:07 -0700

    3-factorial.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * _strlen_recursion - length of string
+ * @s: string
+ * Return: a
+ */
+
+int _strlen_recursion(char *s)
+{
+	int a;
+	
+	a = 0;
+	if (*s)
+	{
+		a += _strlen_recursion(s + 1);
+		a++;
+	}
+	return (a);
+}
